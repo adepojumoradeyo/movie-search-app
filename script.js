da71bb6778aea60876f4657f2500525190fabd44
Phoebe <https://github.com/adepojumoradeyo/movie-search-app>
2026-02-11 23:58:52 -0800

    updated files

--- script.js
+++ script.js
@@ -5,11 +5,12 @@ const baseUrl = "https://www.omdbapi.com/";
 
 // const loading = document.querySelector(".loading");
 // const error = document.querySelector(".error");
-// const results = document.querySelector(".results");
+const results = document.querySelector(".results");
 
 const movieTitle = document.querySelector(".movie_title");
 const searchBtn = document.querySelector(".searchbtn");
 const movieDetails = document.querySelector(".moviedetails");
+const backBtn = document.querySelector(".backbtn");
 
 let searchResults = [];
 
@@ -38,42 +39,44 @@ function searchMovies() {
 }
 
 function renderMovies(movies) {
-  movieDetails.innerHTML = "";
+  results.innerHTML = "";
   movies.forEach((movie) => {
-    movieDetails.innerHTML += `
-      <div class="movie-card" onclick="showMovieDetails('${movie.imdbID}')">
+    results.innerHTML += `
+      <div class="movie-card  bg-white px-4 py-2 text-left  transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover:bg-indigo-200 duration-300 " onclick="showMovieDetails('${movie.imdbID}')">
         <img src="${movie.Poster !== "N/A" ? movie.Poster : "https://via.placeholder.com/200x300?text=No+Image"}">
-        <h3 class="text-white">${movie.Title}</h3>
-        <p class="text-white">${movie.Year}</p>
-        <p class="text-white">${movie.Type}</p>
+        <h1 class="font-bold">${movie.Title}</h1>
+        <p class="font-medium">${movie.Year}</p>
+        <p class="font-medium">${movie.Type}</p>
       </div>
     `;
   });
 }
 
 function showMovieDetails(imdbID) {
+  results.textContent = "";
   movieDetails.innerHTML = "<p>Loading movie details....</p>";
 
   fetch(`${baseUrl}/?apikey=${API_KEY}&i=${imdbID}&plot=full`)
     .then((res) => res.json())
     .then((movie) => {
       movieDetails.innerHTML = `
-        <div class="details">
+        <div class="details bg-white inline-flex flex-col justify-center text-left px-2 py-5 w-4/5">
         <img src="${movie.Poster !== "N/A" ? movie.Poster : ""}" class="w-80 px-5">
-        <h3 class="text-white text-lg>${movie.Title} (${movie.Year})</h3>
-        <p  class="text-white text-sm"><strong>Plot:</strong> ${movie.Plot}</p>
-        <p  class="text-white text-sm"><strong>Director:</strong> ${movie.Director}</p>
-        <p  class="text-white text-sm"><strong>Actors:</strong> ${movie.Actors}</p>
-        <p  class="text-white text-sm"><strong>IMDb Rating:</strong> ⭐ ${movie.imdbRating}</p>
-        <p  class="text-white text-sm"><strong>Runtime:</strong> ⏱ ${movie.Runtime}</p>
-
+        <h3 class="text-lg mt-4 font-bold">${movie.Title} (${movie.Year})</h3>
+        <p  class=" text-sm mt-3"><strong>Plot:</strong> ${movie.Plot}</p>
+        <p  class=" text-sm mt-3"><strong>Director:</strong> ${movie.Director}</p>
+        <p  class=" text-sm mt-3"><strong>Actors:</strong> ${movie.Actors}</p>
+        <p  class=" text-sm mt-3"><strong>IMDb Rating:</strong> ⭐ ${movie.imdbRating}</p>
+        <p  class=" text-sm mt-3"><strong>Runtime:</strong> ⏱ ${movie.Runtime}</p>
         </div>
-      
     `;
+      backBtn.classList.remove("hidden");
     });
 }
 
 searchBtn.addEventListener("click", searchMovies);
+
 function goBack() {
   renderMovies(searchResults);
+  movieDetails.textContent = "";
 }
